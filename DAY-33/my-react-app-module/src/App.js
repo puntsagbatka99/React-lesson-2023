2c507a13cf20321f53b1125a40c959aae456a3cc
Tserenpuntsag Batchuluun <b.tserenpuntsag@Tserenpuntsags-MacBook-Air.local>
2023-01-02 19:06:14 +0800

    update

--- DAY-33/my-react-app-module/src/App.js
+++ DAY-33/my-react-app-module/src/App.js
@@ -2,6 +2,10 @@
 import image1 from './img/image-yellow.png'
 import arrow from "./img/caret-up-fill.svg"
 import profile from "./img/profile1.jpg"
+import image2 from './img/image-aqua.png'
+import image3 from './img/image-steel.png'
+import image4 from './img/image-rose.png'
+import image5 from './img/image.png'
 import './App.css';
 
 function App() {
@@ -14,11 +18,42 @@ function App() {
           <img class="image" src={image1} alt="image"/>
           <div id="contRightSide">
             <div id="smallHeader"><img src={arrow}/> <h4>55</h4></div>
-            <p>Haught or Naught</p>
+            <p id="nameP">Haught or Naught</p>
             <p>High-Minded or absent-minded? You decide.</p>
-            <div id="submit"><p>Submitted by:</p> <img id="profile" src={profile}/> </div>
+            <div id="submit"><p id="submitP">Submitted by:</p> <img id="profile" src={profile}/> </div>
           </div> 
         </div>
+
+        <div id="container">
+          <img class="image" src={image2} alt="image"/>
+          <div id="contRightSide">
+            <div id="smallHeader"><img src={arrow}/> <h4>44</h4></div>
+            <p id="nameP">Yellow Pail</p>
+            <p>On-demand sand castle construction expertise.</p>
+            <div id="submit"><p id="submitP">Submitted by:</p> <img id="profile" src={profile}/> </div>
+          </div> 
+        </div>
+
+        <div id="container">
+          <img class="image" src={image3} alt="image"/>
+          <div id="contRightSide">
+            <div id="smallHeader"><img src={arrow}/> <h4>42</h4></div>
+            <p id="nameP">Tinfoild: Tailored tinfoil hats</p>
+            <p>We already have your measurements and shipping address.</p>
+            <div id="submit"><p id="submitP">Submitted by:</p> <img id="profile" src={profile}/> </div>
+          </div> 
+        </div>
+
+        <div id="container">
+          <img class="image" src={image4} alt="image"/>
+          <div id="contRightSide">
+            <div id="smallHeader"><img src={arrow}/> <h4>23</h4></div>
+            <p id="nameP">Supermajority: The Fantasy Congress League</p>
+            <p>Earn points when your favorite politicians pass legislation.</p>
+            <div id="submit"><p id="submitP">Submitted by:</p> <img id="profile" src={profile}/> </div>
+          </div> 
+        </div>
+
       </header>
     </div>
   );
