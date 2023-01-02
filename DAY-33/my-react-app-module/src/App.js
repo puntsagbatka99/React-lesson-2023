bf28f18455bce018095f8385c73468dc6f755571
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-02 12:36:51 +0800

    popular product

--- DAY-33/my-react-app-module/src/App.js
+++ DAY-33/my-react-app-module/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+
+import image1 from './img/image-yellow.png'
+import arrow from "./img/caret-up-fill.svg"
+import profile from "./img/profile1.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Popular Product</h1>
+        <hr/>
+        <div id="container">
+          <img class="image" src={image1} alt="image"/>
+          <div id="contRightSide">
+            <div id="smallHeader"><img src={arrow}/> <h4>55</h4></div>
+            <p>Haught or Naught</p>
+            <p>High-Minded or absent-minded? You decide.</p>
+            <div id="submit"><p>Submitted by:</p> <img id="profile" src={profile}/> </div>
+          </div> 
+        </div>
       </header>
     </div>
   );
