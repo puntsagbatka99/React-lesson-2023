ef6bb2d58597205587a0c24f2bd31d082f0f2942
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-04 10:03:36 +0800

    update

--- day-35/src/App.js
+++ day-35/src/App.js
@@ -1,22 +1,39 @@
 import './App.css';
 
 function Profile (props){
+  //destructuring
+  const {img, name, age, gender} = props;
   return (
     <div className="profile">
-      <img src={props.img} alt="people"/>
+      {/* <img src={props.img} alt="people"/>
       <p>name: {props.name}</p>
       <p>age: {props.age}</p>
-      <p>gender: {props.gender}</p>
+      <p>gender: {props.gender}</p> */}
+      <img src={img} alt="people"/>
+      <p>name: {name}</p>
+      <p>age: {age}</p>
+      <p>gender: {gender}</p>
     </div>
   )
 }
 
 function App() {
+  // const user = {
+  //   name: "John Smith",
+  //   age: 20,
+  //   gender: "male"
+  // }
   return (
     <div className="App">
       <Profile 
+      // {...user}
         name="John Smith"
         age={20}
+        gender="male"
+      />
+      <Profile 
+        name="Jamal Smith"
+        age={25}
         gender="male"
       />
     </div>
