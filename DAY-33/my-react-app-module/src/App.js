e19d8e9e05c1cff408aa8b8033a747e9941ac06a
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-04 12:02:12 +0800

    update

--- DAY-33/my-react-app-module/src/App.js
+++ DAY-33/my-react-app-module/src/App.js
@@ -1,26 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css';
-import Product from './Product';
-import Product2 from './Product2';
-import Product3 from './Product3';
-import Product4 from './Product4';
-import { ProductFunc, ProductFunc2, ProductFunc3, ProductFunc4 } from "./ProductFunc";
+// import Product from './Product';
+// import Product2 from './Product2';
+// import Product3 from './Product3';
+// import Product4 from './Product4';
+// import { ProductFunc, ProductFunc2, ProductFunc3, ProductFunc4 } from "./ProductFunc";
+import ProductFunc from "./ProductFunc";
+import products from "./seed";
 
 function App() {
+  const productList = products.map((product) =>{
+    console.log(product);
+    return <ProductFunc 
+          title= {product.title}
+          id={product.id}
+          description = {product.description}
+          url = {product.url}
+          votes = {product.votes}
+          submitterAvatarUrl = {product.submitterAvatarUrl}
+          productImageUrl = {product.productImageUrl}
+    />
+  })
   return (
     <div className="App">
       <header className="App-header">
         <h1>Popular Product</h1>
         <hr/>
-        <Product/>
+        {/* <Product/>
         <Product2/>
         <Product3/>
         <Product4/>
         <ProductFunc/>
         <ProductFunc2/>
         <ProductFunc3/>
-        <ProductFunc4/>
+        <ProductFunc4/> */}
+        {productList}
 
       </header>
     </div>
