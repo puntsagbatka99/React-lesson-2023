4ffafa9bf80b5c1d3a00635ab034ddbb11df4ebf
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-06 11:57:33 +0800

    AliceCarousel

--- day-37/src/App.js
+++ day-37/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import AliceCarousel from 'react-alice-carousel'
+import carouselData from './data/carousel';
 function App() {
+
+  const images = carouselData.map(data => {
+    return (
+      <img src={data.name} className={data.style} />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Day - 37 </h1>
+      <AliceCarousel autoPlay autoPlayInterval="3000">
+        {images}
+      </AliceCarousel>
     </div>
+
   );
 }
 
