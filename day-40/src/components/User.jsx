bbafdca23248a5ef5275081085c5f087a4a9cc3d
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-11 09:50:22 +0800

    update React State

---
+++ day-40/src/components/User.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import userList from "../data/data";
+
+export default function User(){
+
+    const [index, setIndex] = useState(0)
+      const [memo, setMemo] = useState(userList[index].name)
+
+    let user = userList[index]
+    return(
+        <div>
+            <button onClick={() => {
+                setMemo(userList[index+1].name)
+                setIndex(index + 1)
+                }}>Next</button>
+            <p>{user.name}</p>
+            <input value={memo}/>
+        </div>
+    )
+}
