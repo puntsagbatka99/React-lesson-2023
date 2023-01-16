483fe1867dab30607e9ee4b963c01e4557bd8975
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-16 10:29:56 +0800

    update

--- day-43/src/App.js
+++ day-43/src/App.js
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { Routes, Route } from "react-router-dom"
 import './App.css';
-import About from './components/About';
-import FeedBackForm from './components/FeedbackForm';
-import LogIn from './components/LogIn';
-import Register from './components/Register';
+import About from "./components/About";
+import FeedBackForm from "./components/FeedbackForm";
+import LogIn from "./components/LogIn";
+import Register from "./components/Register";
+
 
 function App() {
   return (
     <div>
-      <FeedBackForm />
+      <Routes>
+        <Route>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/log-in" element={<LogIn/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/feedback" element={<FeedBackForm/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
