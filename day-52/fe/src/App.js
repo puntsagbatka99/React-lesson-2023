e010b15fb554bdeb0c5b4c2f6a7895501d475285
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-01-30 12:25:04 +0800

    update

--- day-52/fe/src/App.js
+++ day-52/fe/src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import './App.css';
 
 function App() {
@@ -7,42 +7,41 @@ function App() {
 
   const [users, setUsers] = useState([]);
 
-  async function fetchAllData(){
+  async function fetchAllData() {
     // fetch a data from localhost8080
     const FETCHED_DATA = await fetch(URL);
     const FETCHED_JSON = await FETCHED_DATA.json();
     setUsers(FETCHED_JSON.data)
   }
-  
+
   useEffect(() => {
     fetchAllData()
   }, [])
 
-  async function handleSubmit(e){
-    e.preventDefault();
+  async function handleSubmit(e) {
     const postData = {
       username: e.target.username.value,
       age: e.target.age.value,
     }
 
-    const options ={
-      method:"POST",
-      headers:{
+    const options = {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(postData)
     }
 
-    const FETCHED_DATA = await fetch (URL, options)
+    const FETCHED_DATA = await fetch(URL, options)
     const FETCHED_JSON = await FETCHED_DATA.json();
     console.log(FETCHED_JSON)
     setUsers(FETCHED_JSON.data)
   }
 
-  async function handleDelete(userId){
-    const options ={
-      method:"DELETE",
-      headers:{
+  async function handleDelete(userId) {
+    const options = {
+      method: "DELETE",
+      headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
@@ -50,6 +49,24 @@ function App() {
       })
     }
 
+    const FETCHED_DATA = await fetch(URL, options)
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setUsers(FETCHED_JSON.data)
+  }
+
+  async function handleEdit(e) {
+    e.preventDefault();
+    const editData = {
+      username: e.target.username.value,
+      age: e.target.age.value,
+    }
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editData)
+    }
     const FETCHED_DATA = await fetch(URL, options)
     const FETCHED_JSON = await FETCHED_DATA.json();
     setUsers(FETCHED_JSON.data)
@@ -64,7 +81,7 @@ function App() {
         </label>
         <br />
         <label htmlFor="">Age:
-          <input type="text"  name="age"/>
+          <input type="text" name="age" />
         </label>
         <br />
         <button>Submit</button>
@@ -72,10 +89,14 @@ function App() {
       <h3>Users List</h3>
       {
         users && users.map((user, index) => {
-          return(
+          return (
             <div key={index}>
-              <p>{user.username} : {user.age} {""} <button onClick={()=> handleDelete(user.id)}>Delete</button></p>
-              
+              <p>{user.username} : {user.age}
+                {""}
+                <button onClick={() => handleDelete(user.id)}>Delete</button>
+                {" "}
+                <button onClick={handleEdit}>Edit</button></p>
+
             </div>
           )
         })
