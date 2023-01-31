5276f5491b98a10240f54773c2853367d75fdad5
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-01-31 11:39:54 +0800

    update

--- admin-panel-project/be/app.js
+++ admin-panel-project/be/app.js
@@ -1,1 +1,17 @@
 console.log("Its my APP.JS")
+//Import necesarry moduls
+const express = require("express");
+const cors = require("cors")
+const fs = require("fs")
+
+//Configuration of moduls
+
+const app = express();
+const PORT = 8080
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
