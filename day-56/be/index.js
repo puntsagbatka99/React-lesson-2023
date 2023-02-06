c8f62ddb3ca2e6eb5e2d8ea7362749355bc28a6f
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-02-06 10:00:29 +0800

    Timers

---
+++ day-56/be/index.js
@@ -0,0 +1,39 @@
+console.log("Day-56 REST API")
+
+
+const express = require("express")
+const fs = require("fs")
+const cors = require("cors")
+
+const app = express()
+const PORT = 8080
+
+app.use(cors())
+
+app.get("/", (request, response) => {
+    response.send("<h1>Express API is running</h1>")
+})
+
+app.get("/timers", (request, response) => {
+
+    fs.readFile("./data/data.json", "utf-8", (readError, readData) => {
+        if (readError) {
+            response.json({
+                status: "File not found",
+                data: []
+            })
+        }
+
+        const timerData = JSON.parse(readData);
+        console.log(timerData)
+        console.log(typeof timerData)
+        response.json({
+            status: "Success",
+            data: timerData
+        })
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ day-56/fe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import TimersDashboard from "./components/TimersDashboard";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Timer App</h1>
+      <TimersDashboard />
+    </div>
+  );
+}
