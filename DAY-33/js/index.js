2816674404e5e13909af7c86ded9fcbcfad03134
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-02-07 12:31:44 +0800

    update

--- DAY-33/js/index.js
+++ DAY-33/js/index.js
@@ -1,3 +1,12 @@
 import addTwo from "./math.js";
 
 console.log(addTwo (4, 5));
+
+const array = [1,2,3,4,5,6,7,8,9,10]
+let a = 0;
+for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 != 0){
+        a = a + array[i]
+    }
+}
+console.log(a)
