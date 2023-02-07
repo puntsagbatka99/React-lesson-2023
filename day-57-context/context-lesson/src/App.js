62511d663ba640820a4972bcf4ceca2d88f94609
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-02-07 10:59:03 +0800

    update

--- day-57-context/context-lesson/src/App.js
+++ day-57-context/context-lesson/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
+import Heading from './component/Heading';
 import Layout from './component/Layout';
+import Section from './component/Section';
 import { UserProvider } from "./contexts/UserContext";
 
 function App() {
@@ -8,9 +10,28 @@ function App() {
   return (
     <div className="App">
       <h1>DAY-57 React Context</h1>
-      <UserProvider>
-        <Layout/>
-      </UserProvider>
+
+      <Section level={1}>
+        <Heading >Title</Heading>
+        <Section level={2}>
+          <Heading>Heading</Heading>
+          <Heading >Heading</Heading>
+          <Heading>Heading</Heading>
+          <Section level={3}>
+            <Heading >Sub-Heading</Heading>
+            <Heading >Sub-Heading</Heading>
+            <Heading >Sub-Heading</Heading>
+            <Section level={4}>
+              <Heading >Sub-Sub-Heading</Heading>
+              <Heading >Sub-Sub-Heading</Heading>
+              <Heading >Sub-Sub-Heading</Heading>
+            </Section>
+          </Section>
+        </Section>
+      </Section>
+      {/* <UserProvider> */}
+        {/* <Layout/> */}
+      {/* </UserProvider> */}
     </div>
   );
 }
