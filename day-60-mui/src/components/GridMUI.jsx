8636d06130c2c83cc1ee38f2e510676689512c95
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-02-10 10:06:37 +0800

    update

--- day-60-mui/src/components/GridMUI.jsx
+++ day-60-mui/src/components/GridMUI.jsx
@@ -1,7 +1,14 @@
+import  {Grid}  from "@mui/material"
 const GridMUI = () => {
     return(
         <div>
             <h1>DAY-60 MUI GridBox CustomTheme</h1>
+            <main>
+                <Grid container spacing={2}>
+                    <Grid>First Column</Grid>
+                    <Grid>Second Column</Grid>
+                </Grid>
+            </main>
         </div>
     )
 }
