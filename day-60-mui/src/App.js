cf4b4e2f18d6a0ae06617a5851b0cfe75f88585f
puntsagbatka99 <puntsagbatka99@gmail.com>
2023-02-10 10:03:09 +0800

    update

---
+++ day-60-mui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GridMUI from './components/GridMUI';
+
+function App() {
+  return (
+    <div className="App">
+      <GridMUI/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-60-mui/src/components/GridMUI.jsx
@@ -0,0 +1,9 @@
+const GridMUI = () => {
+    return(
+        <div>
+            <h1>DAY-60 MUI GridBox CustomTheme</h1>
+        </div>
+    )
+}
+
+export default GridMUI 
